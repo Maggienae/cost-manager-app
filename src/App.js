e98a4911d744c3a9b165b35903ca863dae3b51f5
maggi <maggien9703@gmail.com>
2023-08-22 15:13:27 +0300

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [sum, setSum] = useState('');
+  const [category, setCategory] = useState('FOOD');
+  const [description, setDescription] = useState('');
+  const [costList, setCostList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newCostItem = { sum, category, description };
+    setCostList([...costList, newCostItem]);
+    setSum('');
+    setCategory('FOOD');
+    setDescription('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Cost Manager</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="sum">Sum:</label>
+          <input
+              type="number"
+              id="sum"
+              value={sum}
+              onChange={(e) => setSum(e.target.value)}
+              required
+          />
+          <br />
+
+          <label htmlFor="category">Category:</label>
+          <select
+              id="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+          >
+            {/* List of categories */}
+          </select>
+          <br />
+
+          <label htmlFor="description">Description:</label>
+          <input
+              type="text"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+          />
+          <br />
+
+          <button type="submit">Add Cost</button>
+        </form>
+        <div id="costList">
+          {costList.map((item, index) => (
+              <div className="cost-item" key={index}>
+                <strong>Category:</strong> {item.category}<br />
+                <strong>Sum:</strong> {item.sum}<br />
+                <strong>Description:</strong> {item.description}
+              </div>
+          ))}
+        </div>
+      </div>
   );
 }
 
