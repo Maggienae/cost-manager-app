b584d24de968acf97ceb04d16935cd220d1a8965
Eden Bar <edenbaron1@gmail.com>
2023-09-14 12:15:58 +0300

    added team names

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,9 @@
-const reportWebVitals = onPerfEntry => {
+/* Eden Bar 209037571
+   Maggie Nae 208612234
+*/
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
